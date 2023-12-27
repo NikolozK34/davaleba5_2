1801b01b25ea857e2933fde6dfea7d1baf50fb26
NikolozK34 <kekelidze97@gmail.com>
2023-12-27 16:47:36 +0400

    Add project files.

---
+++ davaleba5_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace davaleba5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebPage page = new WebPage();
+            page.SetSiteName("Youtube.com");
+            page.SetPath("https://www.youtube.com/");
+            page.SetSiteDescription("Youtube Videos");
+            page.SetIpAddress("100.10.10.11");
+            page.ShowWeb();
+        }
+    }
+}
---
+++ davaleba5_2/WebPage.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace davaleba5_2
+{
+    internal class WebPage
+    {
+        public string _name;
+        public string _path;
+        public string _description;
+        public string _ip;
+
+        public void Webpage(string name, string path, string description, string ip)
+        {
+            name = _name;
+            path = _path;
+            description = _description;
+            ip = _ip;
+        }
+
+        public void ShowWeb()
+        {
+            Console.WriteLine($"Name: {_name}\nPath: {_path}\nDescription: {_description}\nIP: {_ip}");
+        }
+
+        public string GetSiteName()
+        {
+            return _name;
+        }
+
+        public void SetSiteName(string value)
+        {
+            _name = value;
+        }
+
+        public string GetPath()
+        {
+            return _path;
+        }
+
+        public void SetPath(string value)
+        {
+            _path = value;
+        }
+
+        public string GetSiteDescription()
+        {
+            return _description;
+        }
+
+        public void SetSiteDescription(string value)
+        {
+            _description = value;
+        }
+
+        public string GetIpAddress()
+        {
+            return _ip;
+        }
+
+        public void SetIpAddress(string value)
+        {
+            _ip = value;
+        }
+    }      
+ }
+
